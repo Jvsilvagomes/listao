528386db84d2179dd94ed78f104e0129ca8ddafe
murilow <milanmurilo7@gmail.com>
2025-03-31 08:14:45 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

--- Operadores_Logicos/exercicio_01.js
+++ Operadores_Logicos/exercicio_01.js
@@ -1,9 +1,9 @@
 let numb1 = - 12;
 
 if(numb1 >= 0){
-    console.log(`O numero é positivo.`);
+    console.log(`O numero é positivo!.`);
 }else{
-    console.log(`O numero não é positivo.`);
+    console.log(`O numero não é positivo!.`);
 }
 
 // joao victor da silva gomes 28/03/2025 11:50
