a729e0c90ccd117f958238587b419472ac7046c0
murilow <milanmurilo7@gmail.com>
2025-03-31 08:10:40 -0300

    feat (Operadores-Aritiméticos) Calculo do quadrado e do cubo

--- Operadores_Aritmeticos/exercicio_03.js
+++ Operadores_Aritmeticos/exercicio_03.js
@@ -3,6 +3,6 @@ let n = 10;
 quadrado = n ** 2;
 cubo = n ** 3;
 
-console.log(`O quadrado é igual a ${quadrado}, O cubo é de ${cubo}`)
+console.log(`O quadrado é igual ${quadrado}, E o cubo é de ${cubo}`)
 
 // joao victor da silva gomes 28/03/2025 11:23
