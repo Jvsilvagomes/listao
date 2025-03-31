d194c6ee0324eeace51e4903ea12fd41f3026b61
murilow <milanmurilo7@gmail.com>
2025-03-31 08:42:18 -0300

    feat(operadores-logicos): adiciona checagem de idade pra votar

--- Operadores_Logicos/exercicio_02.js
+++ Operadores_Logicos/exercicio_02.js
@@ -1,13 +1,13 @@
-let idade = 5
+let idade = 5;
 
 if(idade >= 1 && idade <= 16){
-    console.log(`Ainda não pode votar!`)
+    console.log(`Ainda não pode votar!`);
 }else if(idade <= -1){
-    console.log(`Idade inválida!`)
+    console.log(`Idade inválida!`);
 }else if(idade >= 70){
-    console.log(`Não é mais obrigatório votar`)
+    console.log(`Não é mais obrigatório votar`);
 }else{
-    console.log(`Já pode votar!`)
+    console.log(`Já pode votar!`);
 }
 
 // João Victor da Silva Gomes 31/03/2025 08:31
---
+++ Operadores_Logicos/exercicio_03.js
@@ -0,0 +1,9 @@
+let numb1 = 9;
+let numb2 = 99;
+let numb3 = 999;
+
+if(numb1 <= 100){
+    console.log(`Não passou de 100!`)
+}else(numb1 > 100)
+    console.log(`Passou de 100!`)
+
