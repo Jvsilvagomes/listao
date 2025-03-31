73ac88dfbadb6f6221cacb51f2960e94378a3458
murilow <milanmurilo7@gmail.com>
2025-03-31 08:04:36 -0300

    feat (Operadores-Aritiméticos) área do círculo

--- Operadores_Aritmeticos/exercicio_05.js
+++ Operadores_Aritmeticos/exercicio_05.js
@@ -1,8 +1,8 @@
 let raio = 7;
-quadrado = raio * raio;
-calculo = quadrado * 3.14;
+circulo = raio * raio;
+calculo = circulo * 3.14;
 
-console.log(`O raio é de ${caculo}!`)
+console.log(`O raio é de ${calculo}!`)
 
 // joao victor da silva gomes 28/03/2025 11:35
 
