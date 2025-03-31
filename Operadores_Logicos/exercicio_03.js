783200f40f658436d0908b81d4276545024c00d4
murilow <milanmurilo7@gmail.com>
2025-03-31 08:55:53 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

--- Operadores_Logicos/exercicio_03.js
+++ Operadores_Logicos/exercicio_03.js
@@ -3,7 +3,19 @@ let numb2 = 99;
 let numb3 = 999;
 
 if(numb1 <= 100){
-    console.log(`Não passou de 100!`)
-}else(numb1 > 100)
-    console.log(`Passou de 100!`)
+    console.log(`Não passou de 100!`);
+}else{
+    console.log(`Passou de 100:)`)
+}
+    if (numb2 <= 100){
+    console.log(`Não passou de 100!`);
+}else{(numb3 > 100)
+    console.log(`Passou de 100!`);
+}
+if (numb3 <= 100){
+    console.log(`Não passou de 100!`);
+}else if(numb3 > 100){
+    console.log(`Passou de 100!`);
+}
 
+// João Victor da Silva Gomes 31/03/2025 08:54
