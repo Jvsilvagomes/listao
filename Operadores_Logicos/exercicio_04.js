0f58414ea5942d9454be1dd579b7538e07382499
murilow <milanmurilo7@gmail.com>
2025-03-31 09:14:43 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Operadores_Logicos/exercicio_04.js
@@ -0,0 +1,10 @@
+let valor1 = true;
+let valor2 = false;
+
+if(valor1 && valor2 == true){
+    console.log(`È verdade`);
+}else{
+    console.log(`É mentira`);
+}
+
+// João Victor da Silva Gomes 31/03/2025 09:13
