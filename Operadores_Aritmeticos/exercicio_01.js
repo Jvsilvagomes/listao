1c5d758941c1e614a1c0a049fb7fe9c359df2a13
murilow <milanmurilo7@gmail.com>
2025-03-31 08:13:22 -0300

    feat(operadores-aritmeticos): adiciona soma de dois números

--- Operadores_Aritmeticos/exercicio_01.js
+++ Operadores_Aritmeticos/exercicio_01.js
@@ -2,6 +2,6 @@ let a = 3;
 let b = 2 ;
  soma = a + b;
 
- console.log(`O resultado da soma é ${soma} 😊`)
+ console.log(`O resultado da soma é de ${soma} 😊`)
 
 //joao victor da silva gomes 28/03/2025 11:08
