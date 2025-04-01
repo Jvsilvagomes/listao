f3301fcc4a650c181fc680b1469d2e7bd0769759
joao <joao.silvassenai@gmail.com>
2025-04-01 20:11:07 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_else/exercicio_03.js
@@ -0,0 +1,15 @@
+let idade = 9
+
+if (idade <= 12 ){
+    console.log(`É uma criança!`);
+}else if(idade >= 13 && idade <= 17){
+    console.log(`É um adolescente!`);
+}else if(idade >= 18 && idade <= 59){
+    console.log(`É um adulto!`);
+}else if(idade >= 60){
+    console.log(`É um idoso`);
+}else{
+    console.log(`Idade inválida!`)
+}
+
+// joao victor da silva gomes 01/04/2025 19:57
