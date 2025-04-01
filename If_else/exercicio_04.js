f51a2ef7d4344a4b42944f0f0aa3092d18427579
joao <joao.silvassenai@gmail.com>
2025-04-01 20:21:19 -0300

    feat(if-else): adiciona verificação de divisibilidade por 3 e 5

---
+++ If_else/exercicio_04.js
@@ -0,0 +1,9 @@
+let numero = 82
+
+if( numero % 3 === 0 && numero % 5 === 0){
+    console.log(`O numero é divisivel por 3 e por 5 ao mesmo tempo`);
+}else{
+    console.log(`Não é divisível por 3 e por 5 ao mesmo tempo`);
+}
+
+// joao victor da silva gomes 01/04/2025 20:19
