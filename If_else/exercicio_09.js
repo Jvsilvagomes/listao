f6c87ff3225920212f3e668b2db6af9ddb509848
joao <joao.silvassenai@gmail.com>
2025-04-03 19:54:18 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_else/exercicio_09.js
@@ -0,0 +1,11 @@
+let temperatura = 12;
+
+    if(temperatura < 15){
+        console.log(`O clima está frio❄️`);
+    }else if(temperatura >= 15 && temperatura <= 25){
+        console.log(`O clima está agradavel!`);
+    }else if(temperatura > 25){
+        console.log(`O clima está quente♨️`);
+    }
+
+    // Joao victor da silva gomes 03/04/2025 19:53
