78413c7600442d875b5313ee1ca807cf1bd0c49a
joao <joao.silvassenai@gmail.com>
2025-04-03 20:11:12 -0300

    feat(if-else): adiciona verificação de ano bissexto

---
+++ If_else/exercicio_10.js
@@ -0,0 +1,9 @@
+let ano = 364
+
+if(ano % 4 === 0 && ano % 100 !== 0 || ano % 400 === 0){
+    console.log(`${ano} é um ano bissexto`)
+}else{
+    console.log(`${ano} não é um ano bissexto`)
+}
+
+// joao victor da silva gomes 03/04/2025 20:10
