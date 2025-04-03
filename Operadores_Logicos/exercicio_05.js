df19deab30e5abb2a8400d9932bd36d3eb9038dd
joao <joao.silvassenai@gmail.com>
2025-04-03 19:17:09 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

--- Operadores_Logicos/exercicio_05.js
+++ Operadores_Logicos/exercicio_05.js
@@ -1,9 +1,9 @@
 let numero = 55
 
     if (numero % 2 == 0 && numero > 10){
-        console.log(`O numero é par e maio que 10!`)
+        console.log(`O numero é par e maio que 10!`);
     }else{
-        console.log(`O ${numero} não atende as condições!!`)
+        console.log(`O ${numero} não atende as condições!!`);
     }
 
 // Joao Victor da Silva Gomes 01/04/2025 17:18
