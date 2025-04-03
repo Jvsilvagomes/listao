fdca4cdb29b5f00a45ed52a36b59c192a578b2dc
joao <joao.silvassenai@gmail.com>
2025-04-03 19:30:26 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

--- If_else/exercicio_07.js
+++ If_else/exercicio_07.js
@@ -1,10 +1,10 @@
-let salario = 1500
-let desconto = salario * 0.10
-let desconto2 = salario * 0.05
+let salario = 1500;
+let desconto = salario * 0.10;
+let desconto1 = salario * 0.05;
 
 if(salario > 2000){
-    console.log(`Seu salário com o desconto de 10% é de R$ ${salario - desconto}!`)
+    console.log(`Seu salário com o desconto de 10% é de R$ ${salario - desconto}!`);
 }else{
-    console.log(`Seu salario com o disconto de 5% é de R$ ${salario - desconto2}!`)
+    console.log(`Seu salario com o disconto de 5% é de R$ ${salario - desconto1}!`);
 }
 // Joao victor da silva gomes 03/04/2025 19:29
