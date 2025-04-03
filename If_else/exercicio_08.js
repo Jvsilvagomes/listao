606ff4d62afa7b807291178bd0ce4af59fb29676
joao <joao.silvassenai@gmail.com>
2025-04-03 19:41:49 -0300

    O menor de 3 Descrição: Pegue 3 números e exiba o menor deles! Capacidade: Utilizar tomada de decisão para elaboração do algoritmo

---
+++ If_else/exercicio_08.js
@@ -0,0 +1,14 @@
+let numb1 = 9;
+let numb2 = 99;
+let numb3 = 999;
+
+if(numb1 < numb2 && numb1 < numb3){
+    console.log(`O primeiro numero é o menor numero entre os 3`);
+}else if(numb2 < numb1 && numb2 < numb3){
+    console.log(`O segundo numero é o menor nuemro entre os 3  `);
+}else if(numb3 < numb1 && numb3 < numb2){
+    console.log(`O terceiro numero é o menor numero entre os 3`);
+}else{
+    console.log(`Todos os numeros sao iguais!`);
+}
+// Joao victor da silva gomes 03/04/2025 19:40
