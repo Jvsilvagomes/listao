59c73b98e374d9feddb9b766a214d24d5b893351
joao <joao.silvassenai@gmail.com>
2025-04-03 19:03:39 -0300

    feat(if-else): adiciona verificação de aprovação por nota

---
+++ If_else/exercicio_05.js
@@ -0,0 +1,11 @@
+let nota = 99
+
+if(nota <= 59){
+    console.log(`Reprovado!!`);
+}else if( nota >= 60 && nota <= 100){
+    console.log(`Aprovado :)`);
+}else{
+    console.log(`Nota inválida`)
+}
+
+// joao victor da silva gomes 01/04/2025 20:39
