c14ef52c43ce065ea4797a5cf963319db3e2c7ab
joao <joao.silvassenai@gmail.com>
2025-04-03 20:50:12 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch_Case/exercicio_02.js
@@ -0,0 +1,44 @@
+let mês = 3;
+
+switch(mês){
+    case 1:
+        console.log(`Janeiro`);
+        break;
+    case 2:
+        console.log(`Fevereiro`);
+        break;
+    case 3:
+        console.log(`Março`);
+        break;
+    case 4:
+        console.log(`Abril`);
+        break;
+    case 5:
+        console.log(`Maio`);
+        break;
+    case 6:
+        console.log(`Junho`);
+        break;
+    case 7:
+        console.log(`Julho`);
+        break;
+    case 8:
+        console.log(`Agosto`);
+        break;
+    case 9:
+        console.log(`Setembro`);
+        break;
+    case 10:
+        console.log(`Outubro`);
+        break;
+    case 11:
+        console.log(`Novembro`);
+        break;
+    case 12:
+        console.log(`Dezembro`);
+        break;
+    default:
+        console.log(`Numero inválido!, digite entre um numero entre 1 e 12.`)
+}
+
+// joao victor da silva gomes 03/04/2025 20:49
