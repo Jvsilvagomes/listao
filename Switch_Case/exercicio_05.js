52c0457c05264acc844a95a6ce5ba2c86b1ffef4
joao <joao.silvassenai@gmail.com>
2025-04-03 21:48:22 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/exercicio_05.js
@@ -0,0 +1,19 @@
+let estação = 2;
+
+switch(estação){
+    case 1:
+        console.log(`Verão`); 
+        break;
+    case 2:
+        console.log(`Primavera`); 
+        break;
+    case 3:
+        console.log(`Outono`); 
+        break;
+    case 4:
+        console.log(`Inverno`); 
+        break;
+default:
+    console.log(`Estação Inválida!`)
+}
+// joao victor da silva gomes 03/04/2025 21:47feat(switch-case): adiciona exibição da estação do ano
