89885c2f21d9418d9bd7a2db6d9fdc5a98edba26
joao <joao.silvassenai@gmail.com>
2025-04-03 21:40:19 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/exercico-04.js
@@ -0,0 +1,22 @@
+let nota = 9
+
+switch(true){
+    case (nota => 9 && nota <= 10):
+        console.log(`A`);
+        break;
+    case (nota => 7 && nota <= 8):
+        console.log(`B`);
+        break;
+    case (nota => 5 && nota <= 6):
+        console.log(`C`);
+        break;
+    case (nota => 3 && nota <= 4):
+        console.log(`D`);
+        break;
+    case (nota => 0 && nota <= 2):
+        console.log(`E`);
+        break;
+default:
+    console.log(`Nota inválida`)
+}
+// joao victor da silva gomes 03/04/2025 21:39
